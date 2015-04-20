e263292ce2277a16c64e38a0f73b513d719b1280
astralenigma <astralenigma@hotmail.com>
2015-04-20 22:24:57 +0100

    Primeiro Upload

---
+++ Lab1ARC/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Lab1ARC
+{
+    class Program
+    {
+        const int NUMERODECONECCOES = 10;
+        static void Main(string[] args)
+        {
+            
+            Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any,6000);
+            socket.Bind(ip);
+            socket.Listen(NUMERODECONECCOES);
+            Console.WriteLine("Waiting...");
+            Socket socket2 = socket.Accept();
+
+            
+                EndPoint ipep = socket2.RemoteEndPoint;
+                Console.WriteLine("Client " + ipep + " connected./n");
+
+                byte[] data = new byte[1024];
+                string mensagemEnviada = "Bem vindo/n";
+                data = Encoding.ASCII.GetBytes(mensagemEnviada);
+                socket2.Send(data);
+                
+            
+
+
+
+            Console.ReadLine();
+        }
+        static void receberMensagens(Socket socket)
+        {
+            do
+            {
+                byte[] data = new byte[1024];
+                socket.Receive(data);
+                string mensagemRecebida = Encoding.ASCII.GetString(data);
+                Console.WriteLine(mensagemRecebida);
+            } while (socket.Connected);
+            Console.WriteLine("Client " + socket.RemoteEndPoint + " disconnected./n");
+
+        }
+    }
+    
+}
---
+++ Lab2Cliente/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Cliente
+{
+    class Program
+    {
+        const int PORTA = 6000;
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, PORTA);
+            
+            socket.Connect(ip);
+            byte[] data = new byte[1024];
+            socket.Receive(data);
+            string mensagemRecebida = Encoding.ASCII.GetString(data);
+            Console.WriteLine(mensagemRecebida);
+
+            Console.ReadLine();
+
+        }
+    }
+}
