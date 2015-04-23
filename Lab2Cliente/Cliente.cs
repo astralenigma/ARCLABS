5dca6802020ca618697829b879f43c0f5dfa1dba
astralenigma <astralenigma@hotmail.com>
2015-04-23 18:48:05 +0100

    Criados métodos para mensagens únicas, criado método conectar para ser fácil de entender o código.

--- Lab2Cliente/Cliente.cs
+++ Lab2Cliente/Cliente.cs
@@ -15,10 +15,13 @@ namespace Lab2Cliente
         {
 
             Socket socket = conectar("127.0.0.1");
-            byte[] data = new byte[1024];
-            socket.Receive(data);
-            string mensagemRecebida = Encoding.ASCII.GetString(data);
-            Console.WriteLine(mensagemRecebida);
+            
+            receberMensagem(socket);
+            //byte[] data = new byte[1024];
+            //socket.Receive(data);
+            //string mensagemRecebida = Encoding.ASCII.GetString(data);
+            //Console.WriteLine(mensagemRecebida);
+
             enviarMensagens(socket);
             socket.Shutdown(SocketShutdown.Both);
             socket.Close();
@@ -27,22 +30,45 @@ namespace Lab2Cliente
         }
         static void enviarMensagens(Socket socket)
         {
-            string mensagem="";
+            string mensagem = "";
             do
             {
-                mensagem = Console.ReadLine();
-                byte[] data = new byte[1024];
-                data = Encoding.ASCII.GetBytes(mensagem);
-                socket.Send(data);
+                mensagem = enviarMensagem(socket);
             } while (mensagem != "exit");
-            
+
         }
+        static String enviarMensagem(Socket socket)
+        {
+            string mensagem = "";
+            mensagem = Console.ReadLine();
+            byte[] data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(mensagem);
+            socket.Send(data);
+            return mensagem;
+        }
+
+        static void receberMensagens(Socket socket)
+        {
+            do
+            {
+                receberMensagem(socket);
+            } while (socket.Connected);
+        }
+
+        static void receberMensagem(Socket socket)
+        {
+            byte[] data = new byte[1024];
+            socket.Receive(data);
+            string mensagemRecebida = Encoding.ASCII.GetString(data);
+            Console.Write(mensagemRecebida);
+        }
+
         static Socket conectar(String ipStr)
         {
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPAddress ipad = IPAddress.Parse(ipStr);
             IPEndPoint ip = new IPEndPoint(ipad, PORTA);
-            
+
             socket.Connect(ip);
 
             return socket;
